947f64bd51584bf17b53d2456148dab18ada20a2
issy20 <gggibraltar86@gmail.com>
2022-10-07 20:25:56 +0900

    delete cache

--- api/prisma/migrations/20220323062233_add_hashed_refresh_token/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `User` ADD COLUMN `hashedRefreshToken` VARCHAR(191) NULL;
--- client/states/currentUser.ts
+++
@@ -1,9 +0,0 @@
-import { makeVar } from '@apollo/client'
-
-interface VarType {
-  id: string
-  accessToken: string
-  refreshToken: string
-}
-
-export const currentUserVar = makeVar<VarType | null | undefined>(undefined)
